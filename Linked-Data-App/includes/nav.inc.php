24aee7cc27c45601fb05ec35a6a884c6f663d2f0
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-26 17:20:25 +0000

    Created boilerplate files

---
+++ Linked-Data-App/includes/nav.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Michael Bruty</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="data.php">Data</a></li>
+      </ul>
+    </div>
+  </nav>
---
+++ Linked-Data-App/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Michael Bruty</title>
+  <meta name="description" content="Linked Data App">
+  <meta name="author" content="Michael Bruty">
+  <?php  include "includes/materialize.inc.php";?> 
+
+  <link rel="stylesheet" href="css/index.css">
+
+</head>
+
+<body>
+<?php include "includes/nav.inc.php" ?>
+</body>
+</html>
