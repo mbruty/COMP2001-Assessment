6af28f0b162507bbca0365488816ea6889b3a6cd
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-27 17:57:41 +0000

    ✨ Added nav and footer

---
+++ Linked-Data-App/includes/footer.inc.php
@@ -0,0 +1,8 @@
+<footer class="page-footer blue lighten-1">
+    <div class="footer-copyright">
+        <div class="container">
+            © 2020 Michael Bruty
+            <!-- <a class="grey-text text-lighten-4 right" href="#!">More Links</a> -->
+        </div>
+    </div>
+</footer>
--- Linked-Data-App/includes/nav.inc.php
+++ Linked-Data-App/includes/nav.inc.php
@@ -1,6 +1,7 @@
 <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Michael Bruty</a>
+<link rel="stylesheet" href="styles/nav.css" />
+    <div class="nav-wrapper blue lighten-1">
+      <a href="#" class="brand-logo" style="margin-left: 15px;">Michael Bruty</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="index.php">Home</a></li>
         <li><a href="data.php">Data</a></li>
