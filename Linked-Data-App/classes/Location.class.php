e0da345511baff9b0733c3b3e785193a0e11b803
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-28 17:43:41 +0000

    Class to store and parse csv line

---
+++ Linked-Data-App/classes/Location.class.php
@@ -0,0 +1,20 @@
+<?php
+class Location {
+    public $type; 
+    public $pm2_5; 
+    public $lat; 
+    public $lon; 
+    public $name;
+
+    public function __construct($row) {
+        // Extract the address as it contains ','
+        $data = preg_split("/\"/", $row);
+        // Split the data without the address
+        $arr = preg_split("/,/", $data[0] . $data[2]);
+        $this->name = $arr[0];
+        $this->type = $arr[4];
+        $this->pm2_5 = $arr[5];
+        $this->lat = $arr[7];
+        $this->lon = $arr[8];
+    }
+}
