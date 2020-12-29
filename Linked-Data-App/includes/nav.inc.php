590dcccd7ad4ed80c6b82b4b5092cec1002f894e
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-29 14:05:26 +0000

    Set name and added href to title

--- Linked-Data-App/includes/nav.inc.php
+++ Linked-Data-App/includes/nav.inc.php
@@ -1,10 +1,10 @@
 <nav>
-<link rel="stylesheet" href="styles/nav.css" />
-    <div class="nav-wrapper blue darken-4">
-      <a href="#" class="brand-logo" style="margin-left: 15px;">Michael Bruty</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="data.php">Data</a></li>
-      </ul>
-    </div>
-  </nav>
+  <link rel="stylesheet" href="styles/nav.css" />
+  <div class="nav-wrapper blue darken-4">
+    <a href="index.php" class="brand-logo" style="margin-left: 15px;">Southwest&nbsp;Air&nbsp;Quality</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="data.php">Data</a></li>
+    </ul>
+  </div>
+</nav>
