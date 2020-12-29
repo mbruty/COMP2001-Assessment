ce41ec92166119e1ec8410a0f63285b9c554863f
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-29 14:10:48 +0000

    Materialize autocomplete text input

---
+++ Linked-Data-App/includes/autocomplete.inc.php
@@ -0,0 +1,7 @@
+<form action="search.php" method="GET">
+    <div class="input-field">
+        <i class="material-icons prefix">search</i>
+        <input type="text" id="autocomplete-input" name="query" class="autocomplete">
+        <label for="autocomplete-input">Search</label>
+    </div>
+</form>
