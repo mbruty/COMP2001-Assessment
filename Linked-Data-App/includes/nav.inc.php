865db72f5300749481480bd6e6561a647f272869
Mike Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-29 22:00:00 +0000

    Added schema tab

--- Linked-Data-App/includes/nav.inc.php
+++ Linked-Data-App/includes/nav.inc.php
@@ -1,10 +1,11 @@
 <nav>
   <link rel="stylesheet" href="styles/nav.css" />
   <div class="nav-wrapper blue darken-4">
-    <a href="index.php" class="brand-logo" style="margin-left: 15px;">Southwest&nbsp;Air&nbsp;Quality</a>
+    <a href="/index.php" class="brand-logo" style="margin-left: 15px;">Southwest&nbsp;Air&nbsp;Quality</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="data.php">Data</a></li>
+      <li><a href="/schema">Schema</a></li>
+      <li><a href="/index.php">Home</a></li>
+      <li><a href="/data.php">Data</a></li>
     </ul>
   </div>
 </nav>
