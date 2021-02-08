666b6110a6b2744d41fced2c66b14058e38549ec
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-02-08 11:39:48 +0000

    Fixed link

--- Linked-Data-App/src/includes/nav.inc.php
+++ Linked-Data-App/src/includes/nav.inc.php
@@ -1,7 +1,7 @@
 <nav>
   <link rel="stylesheet" href="styles/nav.css" />
   <div class="nav-wrapper blue darken-4">
-    <a href="/index.php" class="brand-logo" style="margin-left: 15px;">Southwest&nbsp;Air&nbsp;Quality</a>
+    <a href="./index.php" class="brand-logo" style="margin-left: 15px;">Southwest&nbsp;Air&nbsp;Quality</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li><a href="schema">Schema</a></li>
       <li><a href="index.php">Home</a></li>
